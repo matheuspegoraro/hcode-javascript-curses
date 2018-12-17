f4f363f43fdd34603f02899578d993c7b0f39698
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-12-17 15:59:26 -0200

    Colocando correções da aula 31

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,8 +1,8 @@
 var name     = document.querySelector("#exampleInputName");
-var gender   = document.querySelector("input[name=gender]:checked");
+var gender   = document.querySelectorAll("#form-user-create input[name=gender]:checked");
 var birth    = document.querySelector("#exampleInputBirth");
 var country  = document.querySelector("#exampleInputCountry");
-var email    = document.querySelector("#exampleInputEmail1");
-var password = document.querySelector("#exampleInputPassword1");
+var email    = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
 var photo    = document.querySelector("#exampleInputFile");
-var admin    = document.querySelector("#")
+var admin    = document.querySelector("#admin")
