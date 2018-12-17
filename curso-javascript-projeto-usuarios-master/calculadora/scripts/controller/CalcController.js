8d47c3374958f77ad2a47f88ab72b05e4d227600
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-12-17 18:54:13 -0200

    Aula 13

--- curso-javascript-projeto-usuarios-master/calculadora/scripts/controller/CalcController.js
+++ curso-javascript-projeto-usuarios-master/calculadora/scripts/controller/CalcController.js
@@ -7,20 +7,35 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
-        
         this.setDisplayDateTime()
 
         setInterval(() => {
             this.setDisplayDateTime()
         }, 1000);
-        
+    }
+
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
     }
 
     initButtonsEvents() {
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, 'click drag', e => {
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = "pointer";
+            });
+        });
     }
 
     setDisplayDateTime() {
