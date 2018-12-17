ebff5d36f4cae4c48b96a1ba8b18a611bfa4d19a
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-12-17 17:36:23 -0200

    Acertando parametros

---
+++ curso-javascript-projeto-usuarios-master/calculadora/scripts/controller/CalcController.js
@@ -0,0 +1,38 @@
+class CalcController {
+
+    constructor() {
+
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize() {
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+    
+        displayCalcEl.innerHTML = "4567"
+        dateEl.innerHTML = "01/05/2020"
+        timeEl.innerHTML = "00:00"
+
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor) {
+        this._displayCalc = valor; 
+    }
+
+    get currentDate() {
+        return this._currentDate;
+    }
+
+    set currentDate(valor) {
+        this._currentDate = valor;
+    }
+
+}
